795f93185eeca3fee8566dd8b7a78dd85243ab13
Ryoichi Izumita <r.izumita@caph.jp>
2020-12-05 08:14:00 +0900

    Unit testing with Jest

---
+++ src/parser/stringParser.test.ts
@@ -0,0 +1,16 @@
+import { ParseFailuer, ParseSuccess } from './parseResult'
+import { StringParser } from './stringParser'
+
+describe('StringParser', () => {
+  test.each([
+    ['', '', ''],
+    ['test', 'test', ''],
+    ['test', 'test and…', ' and…']
+  ])('parse succeeded', (literal, input, next) =>
+    expect(new StringParser(literal).parse(input)).toStrictEqual(new ParseSuccess(literal, next))
+  )
+
+  test.each([['test', 'aaa', 'expect: test']])('parse failed ', (literal, input, message) =>
+    expect(new StringParser(literal).parse(input)).toStrictEqual(new ParseFailuer(message, input))
+  )
+})
